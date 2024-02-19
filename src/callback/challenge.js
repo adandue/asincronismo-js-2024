d9f763383de86378315e9a394e782e961742e6e0
adan_due <ing.adanduenas@gmail.com>
2024-02-19 12:10:33 -0600

    Fetch data

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest')
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 const API = 'https://fakestoreapi.com'
 
 const fetchData = (urlApi, callback) => {
@@ -8,12 +8,25 @@ const fetchData = (urlApi, callback) => {
     xhttp.onreadystatechange = function (event) {
         if(xhttp.readyState === 4) {
             if(xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.respondeText))
+                callback(null, JSON.parse(xhttp.responseText))
+            } else {
+                const error = new Error('Error ' + urlApi)
+                return callback(error, null)
             }
-        } else {
-            const error = new Error('Error ' + urlApi)
-            return callback(error, null)
-        }
+        } 
     }
     xhttp.send();
 }
+
+fetchData(`${API}/products`, (error1, data1) => {
+    if(error1) console.error(error1)
+    fetchData(`${API}/products/${data1[0].id}`, (error2, data2) => {
+        if(error2) console.error(error2)
+        fetchData(`${API}/categories/${data2?.category.id}`, (error3, data3) => {
+            if(error3) console.error(error3)
+            console.log(data1[0])
+            console.log(data2.title)
+            console.log(data3.name)
+        })
+    })
+})
